af4f8ebb3a3f8d3abeb223756c22e6e8bbe40287
Andreas Rozek <A.Rozek@gmx.de>
2024-06-13 06:24:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import dts  from 'vite-plugin-dts'
 import path from 'path'
 
 export default defineConfig({
@@ -18,4 +19,7 @@ export default defineConfig({
       }
     }
   },
+  plugins: [
+    dts({ insertTypesEntry: true }),
+  ],
 })
